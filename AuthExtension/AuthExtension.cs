1c070b58367416dd17e10e2897f0257ea8d9110c
tridipkolkata <tridip2000@gmail.com>
2016-12-22 16:47:42 +0530

    EncryptDecrypt logic added

--- AuthExtension/AuthExtension.cs
+++ AuthExtension/AuthExtension.cs
@@ -3,9 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 using System.Web.Services;
 using System.Web.Services.Protocols;
 using AuthHeader;
+using System.Xml;
+using EncryptClass;
 
 namespace AuthExtension
 {
@@ -28,10 +31,26 @@ namespace AuthExtension
 
     public class AuthExtension : SoapExtension
     {
+
+        private Stream inwardStream;
+        private Stream outwardStream;
+        public override Stream ChainStream(Stream stream)
+        {
+            outwardStream = stream;
+            inwardStream = new MemoryStream();
+            return inwardStream;
+        }
+
         public override void ProcessMessage(SoapMessage message)
         {
             var AfterDeserialize = "";
             bool flag = false;
+
+            string soapMsg1;
+            StreamReader readStr;
+            StreamWriter writeStr;
+            XmlDocument xDoc = new XmlDocument();
+
             if (message.Stage == SoapMessageStage.AfterDeserialize)
             {
                 AfterDeserialize = "AfterDeserialize";
@@ -55,6 +74,95 @@ namespace AuthExtension
                 // Fail the call if we get to here. Either the header
                 // isn't there or it contains invalid credentials.
             }
+            else if (message.Stage == SoapMessageStage.BeforeDeserialize)
+            {
+                readStr = new StreamReader(outwardStream);
+                writeStr = new StreamWriter(inwardStream);
+                soapMsg1 = readStr.ReadToEnd();
+                //soapMsg2 = writeStr.ReadToEnd();
+                if (message is System.Web.Services.Protocols.SoapClientMessage)
+                {
+                    // this is called at client side
+                    xDoc.LoadXml(soapMsg1);
+
+                    //						XmlNodeList xSiteID = xDoc.GetElementsByTagName("siteID");
+                    //						xSiteID[0].InnerXml = decrypt(xSiteID[0].InnerXml);
+                    //
+                    //						XmlNodeList xSitePwd = xDoc.GetElementsByTagName("sitePwd");
+                    //						xSitePwd[0].InnerXml = decrypt(xSitePwd[0].InnerXml);
+
+                    XmlNodeList xResult = xDoc.GetElementsByTagName("AuthenticateResult");
+                    xResult[0].InnerXml = decrypt(xResult[0].InnerXml);
+
+                }
+                else if (message is System.Web.Services.Protocols.SoapServerMessage)
+                {
+                    // this is called at server side
+                    xDoc.LoadXml(soapMsg1);
+
+                    XmlNodeList xSiteID = xDoc.GetElementsByTagName("siteID");
+                    xSiteID[0].InnerXml = decrypt(xSiteID[0].InnerXml);
+
+                    XmlNodeList xSitePwd = xDoc.GetElementsByTagName("sitePwd");
+                    xSitePwd[0].InnerXml = decrypt(xSitePwd[0].InnerXml);
+
+                    XmlNodeList xUserID = xDoc.GetElementsByTagName("UserID");
+                    xUserID[0].InnerXml = decrypt(xUserID[0].InnerXml);
+
+                    XmlNodeList xPwd = xDoc.GetElementsByTagName("Password");
+                    xPwd[0].InnerXml = decrypt(xPwd[0].InnerXml);
+                }
+
+                soapMsg1 = xDoc.InnerXml;
+                writeStr.Write(soapMsg1);
+                writeStr.Flush();
+                inwardStream.Position = 0;
+            }
+            else if (message.Stage == SoapMessageStage.AfterSerialize)
+            {
+                inwardStream.Position = 0;
+                readStr = new StreamReader(inwardStream);
+                writeStr = new StreamWriter(outwardStream);
+                soapMsg1 = readStr.ReadToEnd();
+                //soapMsg2 = writeStr.ReadToEnd();
+                if (message is System.Web.Services.Protocols.SoapClientMessage)
+                {
+                    // this is called at client side
+                    xDoc.LoadXml(soapMsg1);
+
+                    XmlNodeList xSiteID = xDoc.GetElementsByTagName("siteID");
+                    xSiteID[0].InnerXml = encrypt(xSiteID[0].InnerXml);
+
+                    XmlNodeList xSitePwd = xDoc.GetElementsByTagName("sitePwd");
+                    xSitePwd[0].InnerXml = encrypt(xSitePwd[0].InnerXml);
+
+                    XmlNodeList xUserID = xDoc.GetElementsByTagName("UserID");
+                    xUserID[0].InnerXml = encrypt(xUserID[0].InnerXml);
+
+                    XmlNodeList xPwd = xDoc.GetElementsByTagName("Password");
+                    xPwd[0].InnerXml = encrypt(xPwd[0].InnerXml);
+
+                }
+                else if (message is System.Web.Services.Protocols.SoapServerMessage)
+                {
+                    // this is called at server side
+                    xDoc.LoadXml(soapMsg1);
+
+                    //						XmlNodeList xSiteID = xDoc.GetElementsByTagName("siteID");
+                    //						xSiteID[0].InnerXml = encrypt(xSiteID[0].InnerXml);
+                    //
+                    //						XmlNodeList xSitePwd = xDoc.GetElementsByTagName("sitePwd");
+                    //						xSitePwd[0].InnerXml = encrypt(xSitePwd[0].InnerXml);
+
+                    XmlNodeList xResult = xDoc.GetElementsByTagName("AuthenticateResult");
+                    xResult[0].InnerXml = encrypt(xResult[0].InnerXml);
+                }
+
+                soapMsg1 = xDoc.InnerXml;
+                writeStr.Write(soapMsg1);
+                writeStr.Flush();
+            }
+
 
             //if (AfterDeserialize == "AfterDeserialize")
             //{
@@ -66,19 +174,33 @@ namespace AuthExtension
             //}
         }
 
-        public override Object GetInitializer(Type type)
-        {
-            return GetType();
-        }
-
-        public override Object GetInitializer(LogicalMethodInfo info,
-            SoapExtensionAttribute attribute)
+        public override object GetInitializer(Type serviceType)
         {
             return null;
         }
 
-        public override void Initialize(Object initializer)
+        public override object GetInitializer(LogicalMethodInfo methodInfo, SoapExtensionAttribute attribute)
         {
+            return null;
+        }
+
+        public override void Initialize(object initializer)
+        {
+            return;
+        }
+
+        private string encrypt(string message)
+        {
+            EncryptClass.EncryptClass ec = new EncryptClass.EncryptClass();
+            string encryStr = ec.custEncrypt(message);
+            return encryStr;
+        }
+
+        private string decrypt(string message)
+        {
+            EncryptClass.EncryptClass ec = new EncryptClass.EncryptClass();
+            string decryptStr = message;
+            return ec.custDecrypt(decryptStr);
         }
     }
 }
